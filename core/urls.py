0637d51dafcd65e8d8f70d6748b63573b60049dc
Otavio <dev.otaviocostao@gmail.com>
2024-07-22 14:30:43 -0300

    Criando dashboard inicial

--- core/urls.py
+++ core/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from estoque import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('inicio/', views.inicioview, name="inicio"),
 ]
--- estoque/views.py
+++ estoque/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def inicioview(request):
+    
+    return render(request, "estoque/home.html", )
